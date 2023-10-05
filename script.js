491a5991e91ae0bf85e5b560143f56dbcdae261d
felix <felixg10@nycstudents.net>
2023-10-05 11:02:10 -0400

    created basic layout and nonfunctional js

---
+++ script.js
@@ -0,0 +1,14 @@
+const sum = document.getElementById('sum')
+const coinDisplays = document.querySelectorAll('span')
+function addCoin(coinType, multiplier) {
+    switch(coinType) {
+        case 'p':
+            break;
+        case 'n':
+            break;
+        case 'd':
+            break;
+        case 'q':
+            break;
+    }
+}
