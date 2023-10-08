ab59c780093d8d93beb137faaaa2a5159bdfdf59
Felix Gerzon <felixg10@nycstudents.net>
2023-10-08 10:01:35 -0400

    improved it lol

--- script.js
+++ script.js
@@ -1,62 +1,40 @@
 const sum = document.getElementById('sum')
-let total = 0, pennies = 0, nickels = 0, dimes = 0, quarters = 0;
+const grid = document.querySelector('.grid-even-columns')
+let total = 0
+const coinCounts = [0, 0, 0, 0]
+const coinValues = [1, 5, 10, 25]
+const plural = ['Pennies', 'Nickels', 'Dimes', 'Quarters']
+const singular = ['Penny', 'Nickel', 'Dime', 'Quarter']
+const coinNumbers = document.querySelectorAll('input')
 const coinDisplays = document.querySelectorAll('span')
 const errorMessage = document.createElement('p')
 let errorPresent = false
 errorMessage.id = "error"
-function addCoin(coinType, multiplier) {
+function addCoin(coinType, multiplier, numCoins = 1) {
     if (errorPresent) {
-        sum.removeChild(errorMessage)
+        grid.removeChild(errorMessage)
         errorPresent = !errorPresent
     }
-    switch(coinType) {
-        case 'p': 
-            if (multiplier > 0 || (multiplier < 0 && pennies > 0)) {
-                pennies += multiplier
-                total += multiplier;
-                sum.textContent = `$`+(total/100).toFixed(2)
-                coinDisplays[1].textContent = `${pennies} Penn${pennies != 1 ? 'ies' : 'y'}: $${(pennies/100).toFixed(2)}`
-            } else {
-                errorMessage.textContent = 'cannot remove more pennies'
-                sum.appendChild(errorMessage)
-                errorPresent = true
-            }
-            break;
-        case 'n':
-            if (multiplier > 0 || (multiplier < 0 && nickels > 0)) {
-                nickels += multiplier
-                total += 5 * multiplier;
-                sum.textContent = `$`+(total/100).toFixed(2)
-                coinDisplays[2].textContent = `${nickels} Nickel${nickels != 1 ? 's' : ''}: $${(nickels/20).toFixed(2)}`
-            } else {
-                errorMessage.textContent = 'cannot remove more nickels'
-                sum.appendChild(errorMessage)
-                errorPresent = true
-            }
-            break;
-        case 'd':
-            if (multiplier > 0 || (multiplier < 0 && dimes > 0)) {
-                dimes += multiplier
-                total += 10 * multiplier;
-                sum.textContent = `$`+(total/100).toFixed(2)
-                coinDisplays[3].textContent = `${dimes} Dime${dimes != 1 ? 's' : ''}: $${(dimes/20).toFixed(2)}`
-            } else {
-                errorMessage.textContent = 'cannot remove more dimes'
-                sum.appendChild(errorMessage)
-                errorPresent = true
-            }
-            break;
-        case 'q':
-            if (multiplier > 0 || (multiplier < 0 && quarters > 0)) {
-                quarters += multiplier
-                total += 25 * multiplier;
-                sum.textContent = `$`+(total/100).toFixed(2)
-                coinDisplays[4].textContent = `${quarters} Quarter${quarters != 1 ? 's' : ''}: $${(quarters/4).toFixed(2)}`
-            } else {
-                errorMessage.textContent = 'cannot remove more quarters'
-                sum.appendChild(errorMessage)
-                errorPresent = true
-            }
-            break;
+    if ((multiplier > 0 || (multiplier < 0 && coinCounts[coinType] >= numCoins)) && (Math.floor(numCoins) == numCoins)) {
+        coinCounts[coinType] += multiplier * numCoins
+        total += multiplier * numCoins * coinValues[coinType]
+        sum.textContent = `$`+(total/100).toFixed(2)
+        coinDisplays[coinType + 1].textContent =
+            `${coinCounts[coinType] != 1 ? plural[coinType] : singular[coinType]}: $${(coinCounts[coinType]*coinValues[coinType]/100).toFixed(2)}`
+    } else {
+        if (numCoins != Math.floor(numCoins)) errorMessage.textContent = `cannot add fractional ${plural[coinType]}`
+        else errorMessage.textContent = `cannot remove more ${plural[coinType]}`
+        alert(errorMessage.textContent.toLowerCase())
+        grid.appendChild(errorMessage)
+        errorPresent = true
     }
+    coinNumbers[coinType].style.width = `${Math.max(0, Math.floor(Math.log10(coinCounts[coinType])))+2}ch`
+    coinNumbers[coinType].value = coinCounts[coinType]
+}
+function changeCoins(coinType) {
+    let deltaCoins = coinNumbers[coinType].value - coinCounts[coinType]
+    addCoin(coinType, Math.abs(deltaCoins)/deltaCoins, Math.abs(deltaCoins))
+}
+function clearCoins(multiplier) {
+    for (let i = 0; i < 4; i++) addCoin(i, multiplier, coinCounts[i])
 }
